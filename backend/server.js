e53cdb27dba8ea470ba370cdd8bce375d47727da
k021104 <krishnachavda1104@gmail.com>
2026-05-29 15:00:58 +0530

    Fix origin issue

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(
   cors({
     // origin: 'http://localhost:5173',
-    origin: 'https://notes-app-three-rose.vercel.app/',
+    origin: 'https://notes-app-three-rose.vercel.app',
     credentials: true
   })
 )
